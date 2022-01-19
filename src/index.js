44106f973e78bd9e2eb252d2c1b368bc3fd00f7c
2016lisali <2016lisali@gmai.com>
2022-01-19 16:01:27 +1000

    delete r

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-if (window.getSelection().rangeCount >= 1) {
-  var r = window.getSelection().getRangeAt(0);
-}
+
 ReactDOM.render(
     <App />,
   document.getElementById('root')
